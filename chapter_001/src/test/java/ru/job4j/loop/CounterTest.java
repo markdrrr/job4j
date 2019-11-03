84707ad21ef655387b9eff05d8dba9148a521f32
markdrrr <markdrrr@gmail.com>
2019-11-03 18:58:06 +0300

    5.1. Подсчет суммы чётных чисел в диапазоне[#193161]

---
+++ chapter_001/src/test/java/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,14 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class CounterTest {
+    @Test
+    public void whenSumEvenNumbersFromOneToTenThenThirty() {
+        int diapason = Counter.add(1,10);
+        assertThat(diapason, is(30));
+    }
+}
