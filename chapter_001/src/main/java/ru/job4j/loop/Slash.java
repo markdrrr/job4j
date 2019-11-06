36600abf1beb97b2d639ab56758d9441e972c9d5
markdrrr <markdrrr@gmail.com>
2019-11-06 20:12:41 +0300

    5.9. Крест в псевдографике. [#193170]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Slash.java
@@ -0,0 +1,45 @@
+package ru.job4j.loop;
+
+/**
+ * 5.9. Крест в псевдографике. [#193170]
+ * @author Andrey Markushin
+ * @version 1.0
+ * @since 06.11.2019
+ */
+public class Slash {
+
+    /**
+     * Рисует крест в консоле
+     * @param size задает размер креста
+     */
+    public static void draw(int size) {
+        for (int row = 0; row < size; row++) {
+            for (int cell = 0; cell < size; cell++) {
+                boolean left = false;
+                if (cell == row) {
+                    left = true;
+                }
+                boolean right = false;
+                if (cell == (size -1) - row)  {
+                    right = true;
+                }
+                if (left) {
+                    System.out.print("0");
+                } else if (right) {
+                    System.out.print("0");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Draw by 3");
+        draw(3);
+        System.out.println("Draw by 5");
+        draw(5);
+    }
+}
