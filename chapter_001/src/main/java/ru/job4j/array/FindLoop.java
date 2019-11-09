1072d41de6f13fda0a6b8784d399fb55f41d79a7
markdrrr <markdrrr@gmail.com>
2019-11-09 13:32:45 +0300

    6.5.3. Сортировка выборкой.[#193185]

--- chapter_001/src/main/java/ru/job4j/array/FindLoop.java
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -42,6 +42,11 @@ public class FindLoop {
         return rst;
     }
 
+    /**
+     * Сортируем данные в массиве
+     * @param data массив чисел
+     * @return возвращает массив в отсортированном виде
+     */
     public static int[] sort(int[] data) {
         for (int i = 0 ;i != data.length; i++) {
             int min = MinDiapason.findMin(data, i, data.length);
