7e90993f3fbf585dc84cd3019391d70a9b852231
markdrrr <markdrrr@gmail.com>
2019-11-09 13:29:21 +0300

    6.5.3. Сортировка выборкой.[#193185]

--- chapter_001/src/main/java/ru/job4j/array/FindLoop.java
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -42,4 +42,16 @@ public class FindLoop {
         return rst;
     }
 
+    public static int[] sort(int[] data) {
+        for (int i = 0 ;i != data.length; i++) {
+            int min = MinDiapason.findMin(data, i, data.length);
+            int index = FindLoop.indexOf(data, min, i, data.length);
+            int temp = data[i];
+            data[i] = data[index];
+            data[index] = temp;
+        }
+        return data;
+    }
+
+
 }
--- chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -9,7 +9,7 @@ public class FindLoopTest {
     @Test
     public void whenArrayHas10Then1() {
         FindLoop find = new FindLoop();
-        int[] input = new int[] {5, 10, 3};
+        int[] input = new int[]{5, 10, 3};
         int value = 10;
         int result = find.indexOf(input, value);
         int expect = 1;
@@ -19,7 +19,7 @@ public class FindLoopTest {
     @Test
     public void whenArrayHas9ThenMinus1() {
         FindLoop find = new FindLoop();
-        int[] input = new int[] {5, 10, 3};
+        int[] input = new int[]{5, 10, 3};
         int value = 9;
         int result = find.indexOf(input, value);
         int expect = -1;
@@ -28,7 +28,7 @@ public class FindLoopTest {
 
     @Test
     public void whenFind3() {
-        int[] input = new int[] {5, 2, 10, 2, 4};
+        int[] input = new int[]{5, 2, 10, 2, 4};
         int value = 2;
         int start = 2;
         int finish = 4;
@@ -39,12 +39,28 @@ public class FindLoopTest {
 
     @Test
     public void whenFind5() {
-        int[] input = new int[] {6, 1, 4, 2, 3, 7, 3, 9};
+        int[] input = new int[]{6, 1, 4, 2, 3, 7, 3, 9};
         int value = 5;
         int start = 4;
         int finish = 6;
         int result = FindLoop.indexOf(input, value, start, finish);
         int expect = -1;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenSort5() {
+        int[] input = new int[] {3, 4, 1, 2, 5};
+        int result[] = FindLoop.sort(input);
+        int[] expect = new int[] {1, 2, 3, 4, 5};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenSort3() {
+        int[] input = new int[] {7, 21, 1};
+        int result[] = FindLoop.sort(input);
+        int[] expect = new int[] {1, 7, 21};
         assertThat(result, is(expect));
     }
 }
