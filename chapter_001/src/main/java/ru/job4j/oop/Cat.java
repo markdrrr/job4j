82a0d8b942453424cef6b7bdff4272be11684974
markdrrr <markdrrr@gmail.com>
2019-11-18 23:47:33 +0300

    1.5. Поля объекта[#193302]
    (cherry picked from commit af081cd27dafc042fc536889d362447e6d712fde)

--- chapter_001/src/main/java/ru/job4j/oop/Cat.java
+++ chapter_001/src/main/java/ru/job4j/oop/Cat.java
@@ -2,14 +2,28 @@ package ru.job4j.oop;
 
 public class Cat {
 
-    public String sound() {
-        String voice = "may-may";
-        return voice;
+    private String name;
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+    private String food;
+    public void show() {
+        System.out.println(this.name + " кушал " + this.food);
+    }
+    public void eat(String meat) {
+        this.food = meat;
     }
 
     public static void main(String[] args) {
-        Cat peppy = new Cat();
-        String say = peppy.sound();
-        System.out.println("Peppy says " + say);
+        System.out.println("There are gav's food.");
+        Cat gav = new Cat();
+        gav.giveNick("Гав");
+        gav.eat("kotleta");
+        gav.show();
+        System.out.println("There are black's food.");
+        Cat black = new Cat();
+        black.giveNick("Черный");
+        black.eat("fish");
+        black.show();
     }
 }
