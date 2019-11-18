0ae90769ea1dd869c52c4780f80b24e7d927a081
markdrrr <markdrrr@gmail.com>
2019-11-19 00:13:39 +0300

    1.6. Взаимодействие объектов.[#193305]
    (cherry picked from commit 4f22838327ef5e2c841ff20a7ccc5467876b67be)

---
+++ chapter_001/src/main/java/ru/job4j/oop/ballstory/Wolf.java
@@ -0,0 +1,6 @@
+package ru.job4j.oop.ballstory;
+
+public class Wolf {
+    public void eat(Ball ball) {
+    }
+}
---
+++ chapter_001/src/main/java/ru/job4j/oop/story/Pioneer.java
@@ -0,0 +1,7 @@
+package ru.job4j.oop.story;
+
+public class Pioneer {
+
+    public void kill(Wolf wolf) {
+    }
+}
