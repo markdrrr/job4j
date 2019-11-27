f4dafe4a8fe82eac9c730f3c65ede7a35ed23e43
markdrrr <markdrrr@gmail.com>
2019-11-27 21:25:07 +0300

    3. Удаление моделей из массива.[#193324]

---
+++ chapter_002/src/main/java/ru/job4j/pojo/Shop.java
@@ -0,0 +1,88 @@
+package ru.job4j.pojo;
+
+/**
+ * 3. Удаление моделей из массива.[#193324]
+ * @author Andrey Markushin
+ * @version 1.0
+ * @since 27.11.2019
+ */
+public class Shop {
+
+    /**
+     * Удаляет объект из массива по индексу
+     * @param products принимает массив
+     * @param index принимает индекс объекта
+     */
+    public  Product[] delete(Product[] products, int index) {
+        products[index] = null;
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            if (product == null && i < products.length - 1) {
+                products[i] = products[i + 1];
+                products[i + 1] = null;
+            }
+        }
+        return products;
+    }
+
+    public static void main(String[] args) {
+        Product products[] = new Product[5];
+        products[0] = new Product("Milk", 10);
+        products[1] = new Product("Bread", 4);
+        products[2] = new Product("Egg", 19);
+
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            //проверяем, что объект не равен null. тк у нас массив не заполнен целиком.
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+
+        System.out.println();
+        System.out.println("Удаляем значение из ячейки с индексом 1");
+        //удаляем значение из ячейки с индексом 1
+        products[1] = null;
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            //проверяем, что объект не равен null. тк у нас массив не заполнен целиком.
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+
+        System.out.println();
+        System.out.println("Записываем  в ячейку с индексом 1 значение ячейки с индексом 2 и удаляем значение из ячейки с индексом 2");
+        //записываем в ячейку с индексом 1 значение ячейки с индексом 2.
+        products[1] = products[2];
+        //удаляем значение из ячейки с индексом 2.
+        products[2] = null;
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            //проверяем, что объект не равен null. так как  у нас массив не заполнен целиком.
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+
+        System.out.println();
+        System.out.println("Удаляем значение из ячейки с индексом 0 с помощью метода");
+        Shop delete = new Shop();
+        delete.delete(products,0);
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            //проверяем, что объект не равен null. тк у нас массив не заполнен целиком.
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+    }
+}
---
+++ chapter_002/src/test/java/ru/job4j/pojo/ShopTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.pojo;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * 3. Удаление моделей из массива.[#193324]
+ * @author Andrey Markushin
+ * @version 1.0
+ * @since 27.11.2019
+ */
+public class ShopTest {
+
+    @Test
+    public void delete() {
+        Product milk = new Product("Milk", 10);
+        Product bread = new Product("Bread", 4);
+        Product egg = new Product("Egg", 19);
+
+        Product products1[] = new Product[3];
+        products1[0] = milk;
+        products1[1] = bread;
+        products1[2] = egg;
+
+        Product products2[] = new Product[3];
+        products2[0] = bread;
+        products2[1] = egg;
+        Shop delete = new Shop();
+        delete.delete(products1,0);
+
+        assertThat(products1, is(products2));
+    }
+}
