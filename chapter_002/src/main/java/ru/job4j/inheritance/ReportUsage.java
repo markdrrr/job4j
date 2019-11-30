908a165e479dc953d6178bf5378ebd25399eb1b3
markdrrr <markdrrr@gmail.com>
2019-11-30 04:12:40 +0300

    5. Аннотация @Override[#193316]

--- chapter_002/src/main/java/ru/job4j/inheritance/ReportUsage.java
+++ chapter_002/src/main/java/ru/job4j/inheritance/ReportUsage.java
@@ -1,6 +1,7 @@
 package ru.job4j.inheritance;
 
 public class ReportUsage {
+
     public static void main(String[] args) {
         JSONReport newReport = new JSONReport();
         String newText = newReport.generate("Report's", "Report's");
