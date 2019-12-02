1617d69b08b770089a409b49d6338d53552fb1a4
markdrrr <markdrrr@gmail.com>
2019-12-02 16:07:44 +0300

    9.1 Рефакторинг теста @Before @After[#193269]

--- chapter_002/src/test/java/ru/job4j/stragery/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/stragery/PaintTest.java
@@ -1,17 +1,71 @@
 package ru.job4j.stragery;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.StringJoiner;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
+ * 9.1 Рефакторинг теста @Before @After[#193269]
  * @author Andrey Markushin
  * @version 1.0
  * @since 02.12.2019
  */
 public class PaintTest {
+    // поле содержит дефолтный вывод в консоль.
+    private final PrintStream stdout = System.out;
+    // буфер для результата.
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("execute after method");
+    }
+
+    @Test
+    public void whenDrawSquareRefactor() {
+        new Paint().draw(new Square());
+        assertThat(
+                this.out.toString(),
+                is(
+                        new StringBuilder()
+                                .append("++++")
+                                .append("+     +")
+                                .append("+     +")
+                                .append("++++")
+                                .append(System.lineSeparator())
+                                .toString()
+                )
+        );
+    }
+
+    @Test
+    public void whenDrawTriangle() {
+        new Paint().draw(new Triangle());
+        assertThat(
+                this.out.toString(),
+                is(
+                        new StringBuilder()
+                                .append("+")
+                                .append("+ +")
+                                .append("+  +")
+                                .append("+++++")
+                                .toString()
+                )
+        );
+    }
+
     @Test
     public void whenDrawSquare() {
         // получаем ссылку на стандартный вывод в консоль.
