eb56578ececdb205077da2a88ecae1cb2cbe815d
markdrrr <markdrrr@gmail.com>
2019-12-11 06:29:01 +0300

    1. Конвертация листа массивов в один лист Integer[#193250].

--- chapter_003/src/main/java/ru/job4j/collection/ConvertList2Array.java
+++ chapter_003/src/main/java/ru/job4j/collection/ConvertList2Array.java
@@ -1,15 +1,40 @@
 package ru.job4j.collection;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
- * 3. Конвертация ArrayList в двухмерный массив[#193247]
+ * 1. Конвертация листа массивов в один лист Integer[#193250].
  *
  * @author Andrey Markushin
  * @version 1.0
  * @since 11.12.2019
  */
 public class ConvertList2Array {
+
+    /**
+     * Метод проходит по всем элементам всех массивов в списке list и добавить их в один общий лист <Integer>.
+     *
+     * @param list список массивов.
+     * @return возвращает один общий лист <Integer>.
+     */
+    public static List<Integer> convert (List<int[]> list) {
+        List<Integer> result = new ArrayList<>();
+        for (int[] arr : list) {
+            for (int value : arr) {
+                result.add(value);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Метод преобразует одномерный список в двух мерный массив.
+     *
+     * @param list список.
+     * @param cells количество элементов в создаваемом массиве.
+     * @return возвращает двух мерный массив.
+     */
     public static int[][] toArray(List<Integer> list, int cells) {
         int groups = (int) Math.ceil((double) list.size() / cells);
         int[][] array = new int[groups][cells];
@@ -38,5 +63,11 @@ public class ConvertList2Array {
             }
             System.out.println();
         }
+        List<int[]> list2 = new ArrayList<>();
+        list2.add(new int[]{1, 2});
+        list2.add(new int[]{3, 4, 5, 6});
+        List<Integer> result = convert(list2);
+        System.out.println(result);
+
     }
 }
