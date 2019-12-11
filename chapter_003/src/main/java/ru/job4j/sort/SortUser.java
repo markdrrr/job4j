1298370750568139f61b9ab6de6f899b801b9087
markdrrr <markdrrr@gmail.com>
2019-12-11 15:43:34 +0300

    2. Сортировка User с использованием Comparator[#193237]

--- chapter_003/src/main/java/ru/job4j/sort/SortUser.java
+++ chapter_003/src/main/java/ru/job4j/sort/SortUser.java
@@ -3,7 +3,7 @@ package ru.job4j.sort;
 import java.util.*;
 
 /**
- * 1. Организовать сортировку User[#193236]
+ * 2. Сортировка User с использованием Comparator[#193237]
  *
  * @author Andrey Markushin
  * @version 1.0
@@ -15,6 +15,27 @@ public class SortUser {
         return new TreeSet<User>(users);
     }
 
+    public static List<User> sortNameLength (List<User> list) {
+        list.sort(new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                return Integer.compare(o1.getName().length(), o2.getName().length());
+            }
+        });
+        return list;
+    }
+
+    public static List<User> sortByAllFields (List<User> list) {
+        list.sort(new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                int result = o1.getName().compareTo(o2.getName());
+                return result != 0 ? result : Integer.compare(o1.getAge(), o2.getAge());
+            }
+        });
+        return list;
+    }
+
     public static void main(String[] args) {
         List <User> users = new ArrayList<>();
         users.addAll( Arrays.asList(
@@ -24,6 +45,8 @@ public class SortUser {
                 )
         );
         System.out.println(users);
+        System.out.println("Sort: " + sort(users));
+        System.out.println("Sort: " + sort(users));
         System.out.println("Sort: " + sort(users));
     }
 }
--- chapter_003/src/test/java/ru/job4j/sort/SortUserTest.java
+++ chapter_003/src/test/java/ru/job4j/sort/SortUserTest.java
@@ -28,7 +28,43 @@ public class SortUserTest {
         expected.add(user1);
         expected.add(user2);
         assertEquals(expected, result);
+    }
 
+    @Test
+    public void sortNameLength() {
+        List <User> users = new ArrayList<>();
+        User user1 = new User("Сергей", 25);
+        User user2 = new User("Иван", 30);
+        User user3 = new User("Юра", 20);
+        users.add(user1);
+        users.add(user2);
+        users.add(user3);
+        ArrayList<User> result = (ArrayList<User>) SortUser.sortNameLength(users);
+        ArrayList<User> expected = new ArrayList<>();
+        expected.add(user3);
+        expected.add(user2);
+        expected.add(user1);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void sortByAllFields() {
+        List <User> users = new ArrayList<>();
+        User user1 = new User("Сергей", 25);
+        User user2 = new User("Иван", 30);
+        User user3 = new User("Юра", 20);
+        User user4 = new User("Юра", 19);
+        users.add(user1);
+        users.add(user2);
+        users.add(user3);
+        users.add(user4);
+        ArrayList<User> result = (ArrayList<User>) SortUser.sortByAllFields(users);
+        ArrayList<User> expected = new ArrayList<>();
+        expected.add(user2);
+        expected.add(user1);
+        expected.add(user4);
+        expected.add(user3);
+        assertEquals(expected, result);
     }
 
 }
