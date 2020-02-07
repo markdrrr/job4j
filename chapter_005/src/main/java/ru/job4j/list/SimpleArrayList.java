44727b1548efb6b84d397dd3d8015021933de173
markdrrr <markdrrr@gmail.com>
2020-02-08 00:13:27 +0300

    5.3.1. Создать динамический список на базе массива.[#223096]

--- chapter_005/src/main/java/ru/job4j/list/SimpleArrayList.java
+++ chapter_005/src/main/java/ru/job4j/list/SimpleArrayList.java
@@ -1,51 +1,98 @@
 package ru.job4j.list;
 
+import java.util.Arrays;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
 /**
- * 5.3.0 Создать метод delete для односвязного списка[#223094]
+ * 5.3.1. Создать динамический список на базе массива.[#223096]
  *
  * @author Andrey Markushin
- * @version 1.1
- * @since 28.01.2020
+ * @version 1.0
+ * @since 08.02.2020
  */
-public class SimpleArrayList<E> {
+public class SimpleArrayList<E> implements Iterable<E> {
 
-    private int size;
-    private Node<E> first;
+    private Object[] container;
+    private int position = 0;
+    private int size = 0;
+    private int modCount = 0;
 
-    /**
-     * Метод вставляет в начало списка данные.
-     */
-    public void add(E data) {
-        Node<E> newLink = new Node<>(data);
-        newLink.next = this.first;
-        this.first = newLink;
-        this.size++;
+    public SimpleArrayList() {
+        this.container = new Object[10];
+        this.size = 10;
+    }
+
+    public SimpleArrayList(int capacity) {
+        this.container = new Object[capacity];
+        this.size = capacity;
+    }
+
+    public int size() {
+        return this.size;
     }
 
     /**
-     * Метод удаляет первый элемент в списке.
-     * @return удаленный элемент.
+     * Метод добавляет элемент в массив.
      */
-    public E delete() {
-        if (size <= 0) {
+    public void add(E value) {
+        ensureCapacity();
+        this.container[position++] = value;
+    }
+
+    /**
+     * Метод проверяет на вместимость массива, если массив заполнен,
+     * создается новый массив размеором в два раза больще и копируется старый.
+     */
+    private void ensureCapacity() {
+        if (size == position) {
+            int newCapacity = size * 2;
+            container = Arrays.copyOf(container, newCapacity);
+            this.size = newCapacity;
+            modCount++;
+        }
+    }
+
+    /**
+     * Метод получает элемент из массива.
+     * @param index позиции элемента.
+     * @throws NoSuchElementException, если яцейка массива пуста по данному индексу.
+     * @return элеммент массива по заданному индексу
+     */
+    public E get(int index) throws NoSuchElementException {
+        outOfArray(index);
+        if (this.container[index] == null) {
+            throw new NoSuchElementException();
+        }
+        return (E) this.container[index];
+    }
+
+    /**
+     * Метод проверяет индекс вне массива.
+     * @param index для проверки.
+     * @throws IndexOutOfBoundsException, если индекс больше размера массивов.
+     */
+    private void outOfArray(int index) throws IndexOutOfBoundsException {
+        if (index >= this.size) {
             throw new IndexOutOfBoundsException();
         }
-        E result = first.data;
-        first = first.next;
-        this.size--;
-        return result;
     }
 
-    /**
-     * Метод получения элемента по индексу.
-     */
-    public E get(int index) {
-        Node<E> result = this.first;
-        for (int i = 0; i < index; i++) {
-            result = result.next;
-        }
-        return result.data;
-    }
+//    /**
+//     * Метод удаляет элемент массива по индексу.
+//     * @param index для удаления.
+//     * @throws IndexOutOfBoundsException, если индекс больше размера массивов.
+//     */
+//    public E delete(int index) throws NoSuchElementException {
+//        if (size <= 0) {
+//            throw new IndexOutOfBoundsException();
+//        }
+//        E result = first.data;
+//        first = first.next;
+//        this.size--;
+//        return result;
+//    }
 
     /**
      * Метод получения размера коллекции.
@@ -54,16 +101,56 @@ public class SimpleArrayList<E> {
         return this.size;
     }
 
-    /**
-     * Класс предназначен для хранения данных.
-     */
-    private static class Node<E> {
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
 
-        E data;
-        Node<E> next;
+            int cursor = 0;
+            int lastRet = -1;
+            int expectedModCount = modCount;
 
-        Node(E data) {
-            this.data = data;
-        }
+            @Override
+            public boolean hasNext() {
+                checkForModifications();
+                boolean valid = false;
+                if (cursor != size && container[cursor] != null) {
+                    valid = true;
+                }
+                return valid;
+            }
+
+            @Override
+            public E next() {
+                checkForModifications();
+                if (!hasNext()) {
+                    throw new IndexOutOfBoundsException();
+                }
+                return (E) container[cursor++];
+            }
+
+            /**
+             * Проверяет контейнер на наличие изменений после инициализации Итератора.
+             *
+             * @throws ConcurrentModificationException если изменено.
+             */
+            private void checkForModifications() throws ConcurrentModificationException {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+            }
+        };
     }
+
+//    /**
+//     * Класс предназначен для хранения данных.
+//     */
+//    private static class Node<E> {
+//
+//        E data;
+//        Node<E> next;
+//
+//        Node(E data) {
+//            this.data = data;
+//        }
+//    }
 }
--- chapter_005/src/test/java/ru/job4j/list/SimpleArrayListTest.java
+++ chapter_005/src/test/java/ru/job4j/list/SimpleArrayListTest.java
@@ -1,11 +1,9 @@
 package ru.job4j.list;
 
-import org.hamcrest.MatcherAssert;
 import org.junit.Test;
 import org.junit.Before;
-import ru.job4j.iterator.Converter;
 
-import java.util.Arrays;
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
@@ -14,37 +12,64 @@ import static org.junit.Assert.assertThat;
 
 public class SimpleArrayListTest {
 
-    private SimpleArrayList<Integer> list;
+    private SimpleArrayList<Integer> result;
 
     @Before
     public void beforeTest() {
-        list = new SimpleArrayList<>();
-        list.add(1);
-        list.add(2);
-        list.add(3);
+        result  = new SimpleArrayList<>();
+        result.add(1);
+        result.add(2);
+        result.add(3);
     }
 
     @Test
-    public void whenAddThreeElementsThenUseGetOneResultTwo() {
-        assertThat(list.get(1), is(2));
+    public void whenAddThenAdded() {
+        result.add(5);
+        assertThat(result.get(3), is(5));
     }
 
-    @Test
-    public void whenAddThreeElementsThenUseGetSizeResultThree() {
-        assertThat(list.getSize(), is(3));
+    @Test (expected = IndexOutOfBoundsException.class)
+    public void whenGetOutOfBoundsThenException() {
+        result.get(11);
     }
 
-    @Test
-    public void whenAddThreeElementsThenUseDeleteResultThree() {
-        assertThat(list.delete(), is(3));
+    @Test (expected = NoSuchElementException.class)
+    public void whenGetNullThanException() {
+        result.get(7);
     }
 
-    @Test(expected = IndexOutOfBoundsException.class)
-    public void whenAddThreeElementsThenUseDeleteResultException() {
-        assertThat(list.delete(), is(3));
-        assertThat(list.delete(), is(2));
-        assertThat(list.delete(), is(1));
-        assertThat(list.delete(), is(0));
+    @Test (expected = ConcurrentModificationException.class)
+    public void whenEnsureCapacityWhenIterateThenException() {
+        result = new SimpleArrayList<>(2);
+        result.add(1);
+        result.add(2);
+        Iterator<Integer> itr = result.iterator();
+        assertThat(itr.next(), is(1));
+        assertThat(itr.next(), is(2));
+        result.add(3);
+        itr.next();
     }
 
+//    @Test
+//    public void whenAddThreeElementsThenUseGetOneResultTwo() {
+//        assertThat(list.get(1), is(2));
+//    }
+
+//    @Test
+//    public void whenAddThreeElementsThenUseGetSizeResultThree() {
+//        assertThat(list.getSize(), is(3));
+//    }
+
+//    @Test
+//    public void whenAddThreeElementsThenUseDeleteResultThree() {
+//        assertThat(list.delete(), is(3));
+//    }
+//
+//    @Test(expected = IndexOutOfBoundsException.class)
+//    public void whenAddThreeElementsThenUseDeleteResultException() {
+//        assertThat(list.delete(), is(3));
+//        assertThat(list.delete(), is(2));
+//        assertThat(list.delete(), is(1));
+//        assertThat(list.delete(), is(0));
+//    }
 }
