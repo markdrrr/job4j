aa0ec0720980a7d3696bc3769a01891227b5eacd
markdrrr <markdrrr@gmail.com>
2020-04-04 19:15:30 +0300

    1. Создать модель User[#223114]

---
+++ chapter_005/src/main/java/ru/job4j/map/User.java
@@ -0,0 +1,50 @@
+package ru.job4j.map;
+
+import java.util.Calendar;
+
+
+/**
+ * 1. Создать модель User[#223114]
+ *
+ * @author Andrey Markushin
+ * @version 1.0
+ * @since 04.04.2020
+ */
+public class User {
+    private String name;
+    private int children;
+    private Calendar birthday;
+
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+
+    public User() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getChildren() {
+        return children;
+    }
+
+    public void setChildren(int children) {
+        this.children = children;
+    }
+
+    public Calendar getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Calendar birthday) {
+        this.birthday = birthday;
+    }
+}
